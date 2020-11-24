30f4f89e065b02d43cd801bed1737e34879e8b2d
MathOak <45424550+MathOak@users.noreply.github.com>
2020-11-23 22:53:32 -0300

    Firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,14 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./user/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'insert',
+    loadChildren: () => import('./user/insert/insert.module').then( m => m.InsertPageModule)
   }
 ];
 
